74aab89061509155ee41ff06ee4f091dcec3a0a0
Israel <Israel.pro1994@gmail.com>
2020-02-01 16:06:46 -0600

    models and some controllers

--- backend/app/controllers/chessplayers_controller.rb
+++ backend/app/controllers/chessplayers_controller.rb
@@ -1,2 +1,19 @@
 class ChessplayersController < ApplicationController
+
+    skip_before_action :authenticated, only: [:index, :show]
+
+    def index 
+        chessplayers = Chessplayer.all
+        render json: chessplayers, include: ['comments', 'statistics']   
+    end
+
+    def show 
+        chessplayer = Chessplayer.find_by(id: params[:id])
+        if chessplayer
+            render json: chessplayer, include: ['comments', 'statistics']
+        else
+            render json: {message: "chessplayer not found"}
+        end
+    end
+
 end
--- backend/app/controllers/comments_controller.rb
+++ backend/app/controllers/comments_controller.rb
@@ -1,2 +1,33 @@
 class CommentsController < ApplicationController
+
+    skip_before_action :authenticated, only: [:index, :show]
+
+    def index
+        comments = Comment.all
+        render json: comments, except: [:created_at, :updated_at]
+    end
+
+    def show
+        comment = Comment.find_by(id: params[:id])
+        if comment 
+            render json: comment, except: [:created_at, :updated_at]
+        else
+            render json: {message: 'Listing not found'}
+        end
+    end
+
+    def create
+        comment = Comment.create(comment_params)
+    end
+
+    def destroy
+        comment = Comment.destroy
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:user_id, :chessplayer_id, :content)
+    end
+
 end
