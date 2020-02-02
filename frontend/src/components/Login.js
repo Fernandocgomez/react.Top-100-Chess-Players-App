c5b00d1efce7fb9940aaf5942253c7fabaf0fe53
fernandocgomez <fernandocgomez@live.com>
2020-02-01 18:43:03 -0600

    register and login static versions are done

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
   constructor(props) {
@@ -12,11 +13,33 @@ class Login extends React.Component {
   render() {
     
     return (
-        <>
+      <div className='signup-container'>
 
-        
+      <div className='signup-box'>
 
-      </>
+        <div className='signup-img-box'>
+          <img className='signup-img' src='https://i.imgur.com/Ocz3pbJ.png'></img>
+        </div>
+
+        <form className='signup-form'>
+          
+          <label className='signup-label'>Username:</label>
+          <input className='signup-input'></input>
+
+          <label className='signup-label'>Password:</label>
+          <input className='signup-input'></input>
+
+          <button className='signup-btn'>Login!</button>
+
+        </form>
+
+        <div className='signup-redirect-box'>
+          <Link className='signup-redirect'>New User? Register Is Free!</Link>
+        </div>
+
+      </div>
+
+    </div>
     );
   }
 }
--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class SignUp extends React.Component {
   constructor(props) {
@@ -12,9 +13,41 @@ class SignUp extends React.Component {
   render() {
     
     return (
-      <div>
-        
+
+      <div className='signup-container'>
+
+        <div className='signup-box'>
+
+          <div className='signup-img-box'>
+            <img className='signup-img' src='https://i.imgur.com/Ocz3pbJ.png'></img>
+          </div>
+
+          <form className='signup-form'>
+            
+            <label className='signup-label'>Username:</label>
+            <input className='signup-input'></input>
+
+            <label className='signup-label'>Email:</label>
+            <input className='signup-input'></input>
+
+            <label className='signup-label'>Password:</label>
+            <input className='signup-input'></input>
+
+            <label className='signup-label'>Confirm Password:</label>
+            <input className='signup-input'></input>
+
+            <button className='signup-btn'>Register!</button>
+
+          </form>
+
+          <div className='signup-redirect-box'>
+            <Link className='signup-redirect'>You Already Have An Account, Login In!</Link>
+          </div>
+
+        </div>
+
       </div>
+
     );
   }
 }
