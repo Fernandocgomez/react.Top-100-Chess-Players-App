2dc7d13d64c7ac45021cb2d7886683f2494d3bdd
fernandocgomez <fernandocgomez@live.com>
2020-02-03 16:34:58 -0600

    push to even with Israel

--- frontend/src/components/PlayerProfile.js
+++ frontend/src/components/PlayerProfile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Line } from 'react-chartjs-2';
+import { Line, Pie } from 'react-chartjs-2';
 
 class PlayerProfile extends React.Component {
     constructor(props) {
